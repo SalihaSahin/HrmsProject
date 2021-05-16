3a0be9d41513e8ae1887f98909e9bbe05854b2b7
SalihaSahin <saliha_sahin_01@hotmail.com>
2021-05-16 19:06:03 +0300

    first commit

---
+++ hrms/src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobTitleDao.java
@@ -0,0 +1,10 @@
+package kodlamaio.hrms.dataAccess.abstracts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import kodlamaio.hrms.entities.concretes.JobTitle;
+//interface interface i extend eder
+//Crud operasyonları hazırlandı.
+public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
+
+}
